9c3c5613322f1264f5fb3f03c7af011d70fb8b09
Alison Silva <64431216+Alisonsilva47@users.noreply.github.com>
2021-06-19 19:10:24 -0300

    Create stopWatch.js

---
+++ StopWatch/stopWatch.js
@@ -0,0 +1,104 @@
+// Example of React Native Timer and Stopwatch
+// https://aboutreact.com/react-native-timer-stopwatch/
+
+// import React in our code
+import React, { useState } from 'react';
+
+// import all the components we are going to use
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+
+//importing library to use Stopwatch and Timer
+import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
+
+const App = () => {
+  const [isTimerStart, setIsTimerStart] = useState(false);
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+  const [timerDuration, setTimerDuration] = useState(90000);
+  const [resetTimer, setResetTimer] = useState(false);
+  const [resetStopwatch, setResetStopwatch] = useState(false);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.sectionStyle}>
+          <Stopwatch
+            laps
+            msecs
+            start={isStopwatchStart}
+            //To start
+            reset={resetStopwatch}
+            //To reset
+            options={options}
+            //options for the styling
+            getTime={(time) => {
+              console.log(time);
+            }}
+          />
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(!isStopwatchStart);
+              setResetStopwatch(false);
+            }}>
+            <Text style={styles.buttonText}>
+              {!isStopwatchStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(false);
+              setResetStopwatch(true);
+            }}>
+            <Text style={styles.buttonText}>RESET</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#808080',
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  sectionStyle: {
+    flex: 1,
+    marginTop: 32,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    backgroundColor: '#99CCFF',
+    padding: 5,
+    borderRadius: 5,
+    flex: 1,
+    fontSize: 15,
+    marginTop: 10,
+  },
+});
+
+const options = {
+  container: {
+    backgroundColor: '#FF0000',
+    padding: 5,
+    borderRadius: 5,
+    width: 200,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: '#FFF',
+    marginLeft: 7,
+  }
+};
